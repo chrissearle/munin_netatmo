478d95f4a7465dcae7eb3488b3d5744455a5a258
Denis Witt <gpkvt@users.noreply.github.com>
2014-05-16 22:09:13 +0200

    Update netatmo_Temperature.php

--- netatmo_Temperature.php
+++ netatmo_Temperature.php
@@ -5,7 +5,7 @@ foreach ($argv as $arg) {
   $e=explode("=",$arg);
   if(count($e)==2)
     $_GET[$e[0]]=$e[1];
-  else
+  else    
     $_GET[$e[0]]=0;
 }
 
@@ -37,8 +37,8 @@ $client->setVariable("password", $test_password);
 
 $helper = new NAApiHelper();
 try {
-    $tokens = $client->getAccessToken();
-
+    $tokens = $client->getAccessToken();        
+    
 } catch(NAClientException $ex) {
     echo "An error happend while trying to retrieve your tokens\n";
     die();
@@ -48,6 +48,9 @@ try {
 $user = $client->api("getuser", "POST");
 
 $devicelist = $client->api("devicelist", "POST");
+
+#print_r($devicelist);
+
 $devicelist = $helper->SimplifyDeviceList($devicelist);
 
 $last_mesures = $helper->GetLastMeasures($client,$devicelist);
@@ -55,22 +58,38 @@ $last_mesures = $helper->GetLastMeasures($client,$devicelist);
 $device=$devicelist["devices"][0];
 $module=$device["modules"][0];
 
+#print_r($devicelist);
 
 if (key_exists("config",$_GET)) {
   echo "graph_title Temperatures\n";
   echo "graph_vlabel Temperature\n";
   echo "graph_info The Temperatures (all Modules)\n";
   echo "graph_scale no\n";
-  echo "graph_category netatmo\n";
+  echo "graph_category netatmo\n"; 
 }
 
+$color=7;
 foreach($last_mesures[0]['modules'] as $module) {
-  if (key_exists("config",$_GET)) {
-    echo $module['module_name']."Temperature.label ".$module['module_name']."\n";
-  } else {
-    echo $module['module_name']."Temperature.value ".floatval($module['Temperature']);
-    echo "\n";
+  if (key_exists("Temperature",$module)) {
+    if (key_exists("config",$_GET)) {
+      echo $module['module_name']."Temperature.label ".$module['module_name']."\n";
+      if (key_exists("CO2",$module)) {
+        echo $module['module_name']."Temperature.warning 15:30\n";
+        echo $module['module_name']."Temperature.critical 10:40\n";    
+        echo $module['module_name']."Temperature.colour COLOUR".$color."\n";
+      } else {
+        echo $module['module_name']."Temperature.warning 3:30\n";
+        echo $module['module_name']."Temperature.critical -5:40\n";    
+        echo $module['module_name']."Temperature.colour COLOUR".$color."\n";    
+      }
+    } else {
+      echo $module['module_name']."Temperature.value ".floatval($module['Temperature']);
+      echo "\n";
+    }
+    $color++;
   }
 }
+
+#print_r($module);
 
 ?>
